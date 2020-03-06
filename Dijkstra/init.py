d83adb25d79eb6d888e172ac04ab9d045e54533b
Adheesh <adheeshchat@gmail.com>
2020-03-06 12:47:16 -0500

    fixed bugs

--- Dijkstra/init.py
+++ Dijkstra/init.py
@@ -6,103 +6,102 @@
 # | | | | (_| | | | |  __/  __/\__ \ | | |
 # \_| |_/\__,_|_| |_|\___|\___||___/_| |_|
 # Date:   2020-03-05 15:28:04
-# Last Modified time: 2020-03-06 01:40:18
+# Last Modified time: 2020-03-06 12:45:52
 
 import numpy as np
+import matplotlib.pyplot as plt
+import math
 import sys
 sys.path.insert(0,'../')
-import matplotlib.pyplot as plt
+
 from aux_fn import *
 from Environment.env import *
 
-
 class Dijkstra():
-	def __init__(self,grid_size,start_x,start_y,goal_x,goal_y,resolution,robot_size):
+	def __init__(self,grid_size,start_x,start_y,goal_x,goal_y,obs_x,obs_y,resolution,robot_size):
 		self.map_x=grid_size[0]
 		self.map_y=grid_size[1]
 		self.sx=start_x
 		self.sy=start_y
 		self.gx=goal_x
 		self.gy=goal_y
-		
+		self.obs_x=obs_x
+		self.obs_y=obs_y
 		self.res=resolution
 		self.rs=robot_size
 
-		self.pre_processing()
-	
-	def pre_processing(self): #
-		self.n_start=Node(round(self.sx/self.res),round(self.sy/self.res),0,-1)
-		self.n_goal=Node(round(self.gx/self.res),round(self.gy/self.res),0,-1)
-		
-		grid=[self.map_x,self.map_y]
-		env=Environment(grid,self.res,self.rs)
-		#self.ox,self.oy=env.grid_map()
-		self.ob_map=env.obstacle_map()
-		self.motion=motion_model()
-		
 	def algorithm(self):
-		open_list,close_list={},{}
-		open_list[self.calc_index(self.n_start,self.map_x,0,0)] = self.n_start
+	    
+	    n_start = Node(round(self.sx / self.res), round(self.sy / self.res), 0.0, -1)
+	    n_goal = Node(round(self.gx / self.res), round(self.gy / self.res), 0.0, -1)
 
-		
-		while True:
-			c_id=min(open_list,key=lambda o:open_list[o].cost)	#Find node of min cost
-			print('a')
-			current=open_list[c_id]								
+	    grid=[self.map_x,self.map_y]
+	    env=Environment(grid,self.res,self.rs)
+	    ob_map=env.obstacle_map()
+	    motion = motion_model()
 
-			plt.plot(current.x*self.res,current.y*self.res,"xc")
-			if len(close_list.keys())%10==0:
-				plt.pause(0.001)
+	    open_list, close_list = {}, {}
+	    open_list[self.calc_index(n_start, self.map_x, 0, 0)] = n_start
+	    
+	    while True:
+	    	c_id = min(open_list, key=lambda o: open_list[o].cost)
+	    	current = open_list[c_id]
+	    	
+	    	plt.plot(current.x * self.res, current.y * self.res, "xc")
+	    	if len(close_list.keys()) % 10 == 0:
+	    		plt.pause(0.001)
 
-			if current.x==self.n_goal.x and current.y==self.n_goal.y:		#Check if goal reached
-				print("found goal")
-				self.n_goal.ind=current.ind
-				self.n_goal.cost=current.cost
-				break
+	    	if current.x == n_goal.x and current.y == n_goal.y:
+	    		print("Found goal")
+	    		n_goal.ind = current.ind
+	    		n_goal.cost = current.cost
+	    		break
 
-			del open_list[c_id]			# Remove the item from the open list
-			close_list[c_id]=current	# Add it to the closed list
+	    	
+	    	del open_list[c_id]			# Remove the item from the open set
+	    	
+	    	close_list[c_id] = current	# Add it to the closed set
 
-			for i,j in enumerate(self.motion):
-				node=Node(current.x+self.motion[i][0],
-							current.y+self.motion[i][1],
-							current.cost+self.motion[i][2],
-							c_id)
-				n_id=self.calc_index(node,self.map_x,0,0)
+	    	# expand search grid based on motion model
+	    	for i, j in enumerate(motion):
+	    		node = Node(current.x + motion[i][0],
+	    			current.y + motion[i][1],
+	    			current.cost + motion[i][2], c_id)
+	    		n_id = self.calc_index(node, self.map_x, 0, 0)
+	    		if n_id in close_list:
+	    			continue
 
-				if not self.verify_node(node,self.ob_map,0,0,self.map_x,self.map_y):	#CHECK COLLISION AND BOUNDARIES
-					continue
+	    		if not self.verify_node(node, ob_map, 0, 0, self.map_x, self.map_y):
+	    			continue
+	    		
+	    		if n_id not in open_list:
+	    			open_list[n_id] = node 
+	    		else:
+	    			if open_list[n_id].cost >= node.cost:
+	    				open_list[n_id] = node
+	    rx, ry = self.calc_final_path(n_goal, close_list, self.res)
+	    return rx, ry
 
-				if n_id in close_list:
-					continue
+	def calc_index(self,node,xwidth,xmin,ymin):
+		return (node.y-ymin)*xwidth + (node.x-xmin)
 
-				if n_id in open_list:
-					if open_list[n_id].cost>node.cost:
-						open_list[n_id].cost=node.cost
-						open_list[n_id].ind=c_id
-				else:
-					open_list[n_id]=node
+	def calc_heuristic(self,n1,n2):
+		w=1.0
+		d=w*math.sqrt((n1.x-n2.x)**2+(n1.y-n2.y)**2)
+		return d
 
-		rx,ry=self.calc_final_path(self.n_goal,close_list,self.res)
-		
-		return rx,ry
-
-	def calc_index(self,node, xwidth, xmin, ymin):
-		return (node.y - ymin) * xwidth + (node.x - xmin)
-
-	def verify_node(self,node,ob_map,xmin,ymin,xmax,ymax):
-		if node.x<xmin:
+	def verify_node(self,node,ob_map,x_min,y_min,x_max,y_max):
+		if node.x<x_min:
 			return False
-		if node.y<ymin:
+		if node.y<y_min:
 			return False
-		if node.x>xmax:
+		if node.x>=x_max:
 			return False
-		if node.y>ymax:
+		if node.y>=y_max:
 			return False
 
 		if ob_map[node.x][node.y]:
 			return False
-
 		return True
 
 	def calc_final_path(self,n_goal,close_list,res):
@@ -114,25 +113,29 @@ class Dijkstra():
 			ry.append(n.y*res)
 			ind=n.ind
 		return rx,ry
-		
-if __name__=="__main__":
-	grid=[30,30]
-	start_x=0
-	start_y=0
 
-	goal_x=20
+
+if __name__=="__main__":
+	grid=[60,60]
+	start_x=5
+	start_y=5
+
+	goal_x=10
 	goal_y=20
 	resolution=1
 	robot_size=1
 
-	d=Dijkstra(grid,start_x,start_y,goal_x,goal_y,resolution,robot_size)
-	rx,ry=d.algorithm()
-	plt.plot(env.obs_x, env.obs_y, "k")
-	plt.scatter(self.start_point[0],self.start_point[1],c='g')
-	plt.scatter(self.goal_point[0],self.goal_point[1],c='r')
-	plt.plot(rx, ry, "-r")
-	# plt.plot(sx, sy, "xr")
-	# plt.plot(gx, gy, "xb")
+	env=Environment(grid,resolution,robot_size)
+	obs_x,obs_y=env.grid_map()
+
+	d=Dijkstra(grid,start_x,start_y,goal_x,goal_y,obs_x,obs_y,resolution,robot_size)
+	
+	plt.plot(obs_x, obs_y, ".k")
+	plt.plot(start_x, start_y, "xr")
+	plt.plot(goal_x, goal_y, "xb")
 	plt.grid(True)
 	plt.axis("equal")
+	
+	rx,ry=d.algorithm()
+	plt.plot(rx, ry, "-r")
 	plt.show()
