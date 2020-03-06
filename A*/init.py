0724f67925e8489701d0a82283ed84880b230f50
Adheesh <adheeshchat@gmail.com>
2020-03-06 02:00:37 -0500

    init commit

--- A*/init.py
+++ A*/init.py
@@ -6,6 +6,27 @@
 # | | | | (_| | | | |  __/  __/\__ \ | | |
 # \_| |_/\__,_|_| |_|\___|\___||___/_| |_|
 # Date:   2020-03-05 15:28:04
-# Last Modified time: 2020-03-05 15:28:14
+# Last Modified time: 2020-03-05 18:56:24
 
 import numpy as np
+
+
+
+class A_star():
+	def __init__(self,grid_size,start_x,start_y,goal_x,goal_y,obs_x,obs_y,resolution,robot_size):
+		self.map_x=grid[0]
+		self.map_y=grid[1]
+		self.sx=start_x
+		self.sy=start_y
+		self.gx=goal_x
+		self.gy=goal_y
+		self.ox=obs_x
+		self.oy=obs_y
+		self.res=resolution
+		self.rs=robot_size
+
+
+	def algo():
+		grid=[self.map_x,self.map_y]
+		env=Environment(grid,self.res,self.rs)
+		ob_map=env.obstacle_map()
