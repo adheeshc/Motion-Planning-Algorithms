668e9b9c092fdf987cc528a391c3afe33f71f878
Adheesh <adheeshchat@gmail.com>
2020-03-06 02:00:51 -0500

    minor changes

--- Environment/test.py
+++ Environment/test.py
@@ -6,17 +6,15 @@
 # | | | | (_| | | | |  __/  __/\__ \ | | |
 # \_| |_/\__,_|_| |_|\___|\___||___/_| |_|
 # Date:   2020-03-05 16:27:57
-# Last Modified time: 2020-03-05 17:09:14
+# Last Modified time: 2020-03-06 01:57:19
 
-from init import *
-import matplotlib.pyplot as plt
+from env import *
 
 if __name__=="__main__":
-	grid=[300,150]
-	#grid=[150,300]
-	#grid=[300,300]
+	#grid=[30,15]
+	#grid=[15,30]
+	grid=[60,60]
 	res=1
 	rob_size=1
 	env=Environment(grid,res,rob_size)
 	env.show_map()
-
